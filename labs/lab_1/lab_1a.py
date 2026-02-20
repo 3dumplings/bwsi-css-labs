870aba05df80dae7f99ff45bcbabb3fbf9d42529
3dumplings <brojellyiscool@gmail.com>
2026-02-20 15:24:22 -0500

    Add introduction lab_1a.py

--- labs/lab_1/lab_1a.py
+++ labs/lab_1/lab_1a.py
@@ -11,6 +11,7 @@ def main():
     name = "Chenyang Xia" # TODO: Insert your name between the double quotes
 
     print(f"{name}, Welcome to the CSS course!")
-
+    print("My name is Chenyang Xia, but I go by Howie. I'm a sophomore from New York, and one fun fact about myself is that I like to play volleyball. Although I started playing late, I love the fact that most of the community is very welcoming, allowing for my rec runs every Saturday. Additionally, I'm considering getting a 3d printer sometime in the future so that I could expand my opportunities and curiosities within the world of engineering. I'm very excited to learn along with you all in this course and possibly make it to the actual summer program!")
+    
 if __name__ == "__main__":
     main()
